71e721979cbcf64423665df2ddaea288df616773
Mikhail Sedov <mikhail.sedov@zf.com>
2024-11-15 19:31:04 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/open-skillz/sw.js', { scope: '/open-skillz/' })})}
